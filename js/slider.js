bafc24f8ac48935c86bd85bed7c5b33b6ef7c44b
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-07-20 20:48:09 +0200

    fix: slider dots

--- js/slider.js
+++ js/slider.js
@@ -8,18 +8,18 @@ $(document).ready(function () {
 
   const slideImageIds = [1, 2, 3]; 
 
-  const sliderContainer = $('.hero-slider');
+  const sliderContainer = $('.slider');
 
   slideImageIds.forEach(imageId => {
     const imageUrl = `https://picsum.photos/1380/550?random=${imageId}`;
 
     const slideHtml = `
-      <div class="hero-slider__item">
-        <img data-lazy="${imageUrl}" class="hero-slider__background" alt="${commonSlideContent.title}">
-        <div class="hero-slider__content">
-          <h2 class="hero-slider__title">${commonSlideContent.title}</h2>
-          <p class="hero-slider__description">${commonSlideContent.description}</p>
-          <a href="#" class="hero-slider__button">${commonSlideContent.buttonText}</a>
+      <div class="slider__item">
+        <img data-lazy="${imageUrl}" class="slider__background" alt="${commonSlideContent.title}">
+        <div class="slider__content">
+          <h2 class="slider__title">${commonSlideContent.title}</h2>
+          <p class="slider__description">${commonSlideContent.description}</p>
+          <a href="#" class="slider__button">${commonSlideContent.buttonText}</a>
         </div>
       </div>
     `;
